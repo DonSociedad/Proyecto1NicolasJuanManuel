1db422eecb8939d65da05d5e1ae56f9cd8a76740
Nicolas Medina <nicofeme2015@gmail.com>
2025-02-02 16:17:42 -0500

    Primer commit

---
+++ InventarioClient/src/main/java/com/mycompany/inventarioclient/InventarioClient.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.inventarioclient;
+
+/**
+ *
+ * @author nmedi
+ */
+public class InventarioClient {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
