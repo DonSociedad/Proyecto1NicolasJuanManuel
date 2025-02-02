2473816606ad645d33c1e9732bffbb2bda4a43db
Juanma El Mejor <juanma.correo2006@gmail.com>
2025-02-01 23:01:53 -0500

    first commit

---
+++ InventarioServer/src/main/java/com/mycompany/inventarioserver/InventarioServer.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.inventarioserver;
+
+/**
+ *
+ * @author Juan Manuel
+ */
+public class InventarioServer {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
