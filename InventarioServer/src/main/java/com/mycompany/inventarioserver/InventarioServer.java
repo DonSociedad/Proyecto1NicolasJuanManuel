99fbcf553e1ac96db5e6c386d840304333b6c483
Juanma El Mejor <juanma.correo2006@gmail.com>
2025-02-02 14:56:26 -0500

    se realizo la conexion server-client basica

--- InventarioServer/src/main/java/com/mycompany/inventarioserver/InventarioServer.java
+++ InventarioServer/src/main/java/com/mycompany/inventarioserver/InventarioServer.java
@@ -4,6 +4,15 @@
 
 package com.mycompany.inventarioserver;
 
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Juan Manuel
@@ -11,6 +20,31 @@ package com.mycompany.inventarioserver;
 public class InventarioServer {
 
     public static void main(String[] args) {
-        System.out.println("Servidor");
+        ServerSocket servidor ;
+        Socket sc=null;
+        DataInputStream in;
+        DataOutputStream out;
+        
+        int puerto = 5051;
+        
+        try {
+            servidor = new ServerSocket(puerto);
+            System.out.println("Servidor Iniciado en el puerto "+ puerto);
+            while (true) {
+                sc = servidor.accept();
+                System.out.println("Cliente encontrado");
+                
+                in = new DataInputStream(sc.getInputStream());
+                out = new DataOutputStream(sc.getOutputStream());
+                String message = in.readUTF();
+                System.out.println(message);
+                out.writeUTF("Que tal desde el servidor");
+                sc.close();
+                System.out.println("cliente desconectado...");
+            }
+
+        } catch (IOException ex) {
+            Logger.getLogger(InventarioServer.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
