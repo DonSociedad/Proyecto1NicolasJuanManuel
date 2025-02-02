0a53e6b08f5d4c859232f677d0e3d87253274df5
Juanma El Mejor <juanma.correo2006@gmail.com>
2025-02-02 16:13:56 -0500

    se documento lo que estaba en el commit anterior # Conflicts: #	README.md

--- InventarioServer/src/main/java/com/mycompany/inventarioserver/InventarioServer.java
+++ InventarioServer/src/main/java/com/mycompany/inventarioserver/InventarioServer.java
@@ -20,25 +20,65 @@ import java.util.logging.Logger;
 public class InventarioServer {
 
     public static void main(String[] args) {
+        /**
+         *se crea el socket para el servidor
+         */
+        
         ServerSocket servidor ;
+        /**
+         * socket del cliente junto a input y output
+         */
         Socket sc=null;
         DataInputStream in;
         DataOutputStream out;
         
+        /**
+         * puerto en el que se va a trabajar
+         */
         int puerto = 5051;
         
+        /**
+         * try-catch para las excepciones de socket
+         */
         try {
+            /**
+            * se inicializa el servidor
+            */
             servidor = new ServerSocket(puerto);
             System.out.println("Servidor Iniciado en el puerto "+ puerto);
+            /**
+            * ciclo infinito para mantener al servidor siempre al pendiente de cualquier solicitud
+            */
             while (true) {
+                /**
+                * el servidor espera una conexion con un cliente
+                */
                 sc = servidor.accept();
                 System.out.println("Cliente encontrado");
                 
+                /**
+                *se inicializa in para recibir informacion
+                */
                 in = new DataInputStream(sc.getInputStream());
+                /**
+                * se inicializa el out para enviar informacion
+                */
                 out = new DataOutputStream(sc.getOutputStream());
+                
+                /**
+                * se crea la variable mensaje para esperar y recibir un mensaje del cliente
+                */
                 String message = in.readUTF();
                 System.out.println(message);
+                
+                /**
+                * envia un mensaje al cliente cuando este ya recibio su mensaje
+                */
                 out.writeUTF("Que tal desde el servidor");
+                
+                /**
+                * se cierra la conexion con el cliente
+                */
                 sc.close();
                 System.out.println("cliente desconectado...");
             }
